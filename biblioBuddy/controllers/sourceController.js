030fdc7911ebabbf0bb5051609fcdfb2eab82c02
Robert Kwan <rkwan94@gmail.com>
2015-12-26 23:10:33 +1100

    finished chicago style citations

--- biblioBuddy/controllers/sourceController.js
+++ biblioBuddy/controllers/sourceController.js
@@ -146,56 +146,69 @@ app.controller('sourceController', function(){
 
 	this.chicago = function(){
 		var inTextAuthor = this.source.authors[0].firstName, " ", this.source.authors[0].lastName;
+		var refAuthor = this.source.authors[0].lastName, ", ", this.source.authors[0].firstName;
 		var numAuthors = this.source.authors.length;
 
-		if(numAuthors < 4){
-			
-			for(var i = 1; i < numAuthors-1; i++){
-				inTextAuthor = inTextAuthor.concat(", ", this.source.authors[i].firstName, " ", this.source.authors[i].lastName);
+		if(type != 'website'){
+			if(numAuthors < 4){
+				for(var i = 1; i < numAuthors-1; i++){
+					inTextAuthor = inTextAuthor.concat(", ", this.source.authors[i].firstName, " ", this.source.authors[i].lastName);
+					refAuthor = refAuthor.concat(", ", this.source.authors[i].firstName, " ", this.source.authors[i].lastName);
+				}
+	
+				inTextAuthor = inTextAuthor.concat(" and ", this.source.authors[numAuthors-1].firstName, " ", this.source.authors[numAuthors-1].lastName, ", ");
+			}else{
+				for(var i = 1; i < numAuthors-1; i++){
+					refAuthor = refAuthor.concat(", ", this.source.authors[i].firstName, " ", this.source.authors[i].lastName);
+				}
+				inTextAuthor = inTextAuthor.concat(" et al., ");
 			}
-
-			inTextAuthor = inTextAuthor.concat(" and ", this.source.authors[numAuthors-1].firstName, " ", this.source.authors[numAuthors-1].lastName, ", ");
-		}else{
-			inTextAuthor = inTextAuthor.concat(" et al., ");
 		}
-		
 		this.source.inText = this.source.inText.concat(inTextAuthor);
-
-		if(type == 'website'){
-			this.source.reference = this.source.hasOwnProperty('author') ? this.referenceAuthorCitation() : '';
-			this.source.reference = this.source.hasOwnProperty('year') ? this.source.reference.concat(" ", this.source.year, ", ") : this.source.reference.concat(" n.d. ");
-		}else{
-			this.source.reference = this.referenceAuthorCitation();
-			this.source.reference = this.source.reference.concat(" ", this.source.year, ", ");
-		}
+		this.source.reference = this.source.reference.concat(refAuthor);
 
 
-		if(type == 'book'){	
+		if(type == 'book'){
+			this.source.inText = this.source.inText.concat("<i>", this.source.title, "</i> ");
+			this.source.inText = this.source.inText.concat("(", this.source.cityPublished, ": ");
+			this.source.inText = this.source.inText.concat(this.source.publisher, ", ");
+			this.source.inText = this.source.inText.concat(this.source.year, "), ");
+			this.source.inText = this.source.inText.concat(this.source.pagesUsed, ".");
+
 			this.source.reference = this.source.reference.concat("<i>", this.source.title, "</i>. ");
 			this.source.reference = this.source.reference.concat(this.source.cityPublished, ": ");
 			this.source.reference = this.source.reference.concat(this.source.publisher, ", ");
 			this.source.reference = this.source.reference.concat(this.source.year, ".");
 		}else if(type == 'journalArticle'){
-			this.source.reference = this.source.reference.concat("'", this.source.title, "', ");
-			this.source.reference = this.source.reference.concat("<i>", this.source.journal, "</i>, ");
-			this.source.reference = this.source.reference.concat("vol. ", this.source.journalNum, ", ");
-			this.source.reference = this.source.reference.concat("no. ", this.source.issueNum, ", ");
-			this.source.reference = this.source.reference.concat("pp. ", this.source.pagesUsed, ".");
+			this.source.inText = this.source.inText.concat(', "', this.source.articleTitle, '," ');
+			this.source.inText = this.source.inText.concat("<i>", this.source.journalArticle, "</i> ");
+			this.source.inText = this.source.inText.concat(this.source.journalNum, ", ");
+			this.source.inText = this.source.inText.concat("no. ", this.source.issueNum, " (");
+			this.source.inText = this.source.inText.concat(this.source.year, "): ");
+			this.source.inText = this.source.inText.concat(this.source.pagesUsed, ".");
+
+			this.source.reference = this.source.reference.concat(', "', this.source.articleTitle, '," ');
+			this.source.reference = this.source.reference.concat("<i>", this.source.journalArticle, "</i> ");
+			this.source.reference = this.source.reference.concat(this.source.journalNum, ", ");
+			this.source.reference = this.source.reference.concat("no. ", this.source.issueNum, " (");
+			this.source.reference = this.source.reference.concat(this.source.year, "): ");
+			this.source.reference = this.source.reference.concat(this.source.pagesUsed, ".");
 		}else if(type == 'website'){
-			this.source.reference = this.source.reference.concat("<i>", this.source.pageTitle, "</i>, ");
-			this.source.reference = this.source.reference.concat(this.source.siteName, ", ");
-			this.source.reference = this.source.reference.concat("viewed ", this.source.date.getDate(), " ", monthNames[this.source.date.getMonth()], " ", this.source.date.getFullYear(), ", ");
-			
-			if(refStyle == "uts"){
-				this.source.reference = this.source.reference.concat("<", this.source.url, ">");
-			}else{
-				this.source.reference = this.source.reference.concat(this.source.url);
-			}
-			
+			this.source.inText = this.source.inText.concat('"', this.source.pageTitle, '," ');
+			this.source.inText = this.source.inText.concat(this.source.siteName, ", ");
+			this.source.inText = this.source.inText.concat("Date accessed ", this.source.date.getDate(), " ", monthNames[this.source.date.getMonth()], ", ", this.source.date.getFullYear(), ".");
+
+			this.source.reference = this.source.inText;
+			this.source.reference = this.source.reference.concat(" ", this.source.url, ".");
 		}else if(type == 'newsOrMag'){
-			this.source.reference = this.source.reference.concat("'", this.source.articleTitle, "', ");
-			this.source.reference = this.source.reference.concat("<i>", this.source.paperName, "</i>, ");
-			this.source.reference = this.source.reference.concat(this.source.date.getDate(), " ", monthNames[this.source.date.getMonth()]);
+			this.source.inText = this.source.inText.concat(', "', this.source.articleTitle, '," ');
+			this.source.inText = this.source.inText.concat("<i>", this.source.paperName, "</i> (");
+			this.source.inText = this.source.inText.concat(this.source.date.getDate(), " ", monthNames[this.source.date.getMonth()], "): ");
+			this.source.inText = this.source.hasOwnProperty('pagesUsed') ? this.source.reference.concat(", ", this.source.pagesUsed, ".") : this.source.reference.concat(".");
+
+			this.source.reference = this.source.reference.concat(', "', this.source.articleTitle, '," ');
+			this.source.reference = this.source.reference.concat("<i>", this.source.paperName, "</i> (");
+			this.source.reference = this.source.reference.concat(this.source.date.getDate(), " ", monthNames[this.source.date.getMonth()], "): ");
 			this.source.reference = this.source.hasOwnProperty('pagesUsed') ? this.source.reference.concat(", ", this.source.pagesUsed, ".") : this.source.reference.concat(".");
 		}
 	}
@@ -314,10 +327,11 @@ app.controller('sourceController', function(){
 	this.search = function(query){
 		var xmlhttp = new XMLHttpRequest();
 		var key = "vqI5LRdlC8eFJARJqH2yjB27CmDPcMkgp3bc9mnRkfQbUS9micwMTpPjhSF5N4KyIp2Bo8QKSHolMgrA"
-		var url = String.concat("http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=", query, "&wskey=", key);
+		var searchUrl = String.concat("http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=", query, "&wskey=", key);
+		var citeUrl = String.concat("http://www.worldcat.org/webservices/catalog/content/citations/", queryOCLCNum, "?wskey=", key, "", );
 
 
-		xmlhttp.open("GET", url, true);
+		xmlhttp.open("GET", searchUrl, true);
 		xmlhttp.send();
 	};
 
